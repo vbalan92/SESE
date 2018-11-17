187d20ef03fff8b691a43d8aa54dd45150e94564
Kamil Sierzant <sierzant.kamil@gmail.com>
2018-11-17 16:29:45 +0100

    * reserve room partially implemented * use ng-bootstrap instead of bootstrap

---
+++ user-portal/frontend/src/app/services/reservation.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {ReservationDTO} from "../hotel-management/room-list/models/reservation";
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+  private reservationUrl = 'http://localhost:8080/reservations';
+
+  constructor(private http: HttpClient) {
+  }
+
+  reserveRoom(reservation: ReservationDTO): Observable<ReservationDTO> {
+    return this.http.post<ReservationDTO>(this.reservationUrl, reservation, httpOptions);
+
+  }
+}
