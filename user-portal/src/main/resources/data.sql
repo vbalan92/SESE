0907b931b3885d29b4c02d3f2e2147bd930b0267
Vlad - Ioan Balan <e1126441@student.tuwien.ac.at>
2018-11-18 17:52:47 +0100

    Switch from Postgres DB to H2 in Memory DB for test and app

---
+++ user-portal/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into roles (name) VALUES ('ROLE_USER'), ('ROLE_PM'), ('ROLE_ADMIN');
--- user-portal/src/main/resources/static/data.sql
+++
@@ -1,1 +0,0 @@
-create table customer (id integer not null auto_increment, email varchar(255), first_name varchar(255), last_name varchar(255), primary key (id))
