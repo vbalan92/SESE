0f31e0c844f7a4e5e4a0d8121fdbf18102ecd04b
abbas <abbasulusoy@hotmail.com>
2018-11-21 22:32:56 +0100

    contact service

---
+++ user-portal/frontend/src/app/services/contact.service.ts
@@ -0,0 +1,38 @@
+
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Contact } from '../models/contact.model';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class ContactService {
+
+  constructor(private http: HttpClient) {}
+
+  private contactURL = './api/contacts';
+
+  public getContacts() {
+    return this.http.get<Contact[]>(this.contactURL);
+  }
+
+  public deleteContact(contact) {
+    return this.http.delete(this.contactURL + '/' + contact.id);
+  }
+
+  public createContact(contact) {
+    return this.http.post<Contact>(this.contactURL, contact);
+  }
+
+  public getContact(contact) {
+    return this.http.get<Contact>(this.contactURL + '/' + contact.id);
+  }
+
+  public updateContact(contact) {
+    return this.http.put<Contact>(this.contactURL + '/' + contact.id , contact);
+  }
+}
