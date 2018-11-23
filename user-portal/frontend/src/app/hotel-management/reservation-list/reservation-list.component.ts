4a123fab9f428e0fdef63224052ceee3e983a09e
abbas <abbasulusoy@hotmail.com>
2018-11-23 23:33:25 +0100

    status

--- user-portal/frontend/src/app/hotel-management/reservation-list/reservation-list.component.ts
+++ user-portal/frontend/src/app/hotel-management/reservation-list/reservation-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ReservationDTO} from '../room-list/models/reservation';
+import {ReservationDTO, Status} from '../room-list/models/reservation';
 import {ReservationService} from '../../services/reservation.service';
 import {ReservationDetailDTO} from "./reservation-list-item/models/reservation.dto";
 
@@ -12,6 +12,13 @@ export class ReservationListComponent implements OnInit {
 
   reservations: ReservationDetailDTO[];
 
+  searchReservationDetailDTO: ReservationDetailDTO;
+  status = Status;
+  keys: Status[];
+
+  constructor() {
+   this.keys = Object.keys(this.status);
+  }
   constructor(private reservationService: ReservationService) {
   }
 
@@ -21,5 +28,12 @@ export class ReservationListComponent implements OnInit {
         this.reservations = reservations;
       }
     );
+  }
+
+  searchReservations() {
+    this.reservations.filter(item => item.roomName === this.searchReservationDetailDTO.roomName ||
+      item.roomName === this.searchReservationDetailDTO.roomName ||
+      item.status === this.searchReservationDetailDTO.status ||
+      item.fromDate === this.searchReservationDetailDTO.fromDate);
   }
 }
